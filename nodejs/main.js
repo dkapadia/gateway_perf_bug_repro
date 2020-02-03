d5d6b62fc823da03e2ffe220dfae332b39690a6a
Dhruv Kapadia <git@dkapadia.com>
2020-02-03 14:31:14 -0500

    Set the port

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const asyncHandler = require("express-async-handler")
 const fetch = require("node-fetch");
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 function logResponseTime(req, res, next) {
   const startHrTime = process.hrtime();

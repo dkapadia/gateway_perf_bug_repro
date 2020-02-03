176678a78a7bc812a272af35dcd103148da8aaf9
Dhruv Kapadia <git@dkapadia.com>
2020-02-03 13:52:22 -0500

    Add an endpoint

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,9 +1,54 @@
 const express = require('express')
+const asyncHandler = require("express-async-handler")
+const fetch = require("node-fetch");
 const app = express()
 const port = 3000
 
-app.get("/kaprod", (req, res) =>
-    res.send("OK")
-)
+function logResponseTime(req, res, next) {
+  const startHrTime = process.hrtime();
+
+  res.on("finish", () => {
+    const elapsedHrTime = process.hrtime(startHrTime);
+    const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+    if(elapsedTimeInMs > 20000){
+        console.log("%s : %fms", req.path, elapsedTimeInMs);
+    }
+  });
+
+  next();
+}
+
+// We don't want this in prod
+//app.use(logResponseTime)
+
+app.get("/kaprod", asyncHandler(async (req, res) => {
+    const url = "https://mobile-data-dot-khan-academy.appspot.com/backend-graphql/testDhruv"
+    const expected = JSON.stringify({isOnZeroRatedNetwork: false})
+
+    const body = JSON.stringify({
+        query:"{isOnZeroRatedNetwork}"
+    })
+
+    const options = {
+        body,
+        method: "POST",
+        headers:  {
+         'accept-encoding': 'gzip, deflate, br',
+         'content-type': 'application/json',
+         accept: "application/json"
+        }
+    }
+    const httpResponse = await fetch(url, options)
+
+    const actual = await httpResponse.json()
+
+    if(JSON.stringify(actual.data) != expected){
+        res.status(500);
+        res.send("ERROR");
+        return
+    }
+    res.send("pong\n");
+}))
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
